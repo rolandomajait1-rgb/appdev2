2c39451714b87419f1296299be35fea4ac7b1693
rolandomajait1-rgb <rolandomajait1@gmail.com>
2026-02-17 09:08:58 +0800

    Done with align self section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -1,11 +1,26 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
+import AlignSelfSection from './components/AlignSelfSection';
+import AlignContentSection from './components/AlignContentSection';
+import FlexWrapSection from './components/FlexWrapSection';
+import JustifyContentSection from './components/JustifyContentSection';
+import AlignItemsSection from './components/AlignItemsSection';
+import FlexDirectionSection from './components/FlexDirectionSection';
 import FlexSection from './components/FlexSection';
+import LayoutDirectionSection from './components/LayoutDirectionSection';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <AlignSelfSection />
+      <AlignContentSection />
+      <FlexWrapSection />
+      <JustifyContentSection />
+      <AlignItemsSection />
+      <FlexDirectionSection />
       <FlexSection />
+      <LayoutDirectionSection />
     </View>
   );
 }
