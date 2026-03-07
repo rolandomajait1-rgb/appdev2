75f12b8bafcc61f791b0d14234e019603270546e
rolandomajait1-rgb <rolandomajait1@gmail.com>
2026-03-07 08:16:21 +0800

    Implemented Todo screen with add, toggle, delete, and search functionality

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -2,18 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import Login from './screens/Login';
 import Signup from './screens/Signup';
+import Todo from './screens/Todo';
 
 export default function App() {
-  const [screen, setScreen] = useState('login');
 
   return (
     <>
-      {screen === 'login' ? (
-        <Login onNavigate={() => setScreen('signup')} />
-      ) : (
-        <Signup onNavigate={() => setScreen('login')} />
-      )}
-      <StatusBar style="auto" />
+
+      <Todo />
+
     </>
+
   );
 }
